fb7d3dbb95055810a736b66582a6e06f5f70778a
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-10-24 17:17:17 +0200

    snack-3

--- snack_3/main.js
+++ snack_3/main.js
@@ -3,14 +3,19 @@ const stringa = 'ciao';
 const ordinata = [];
 const reverse = [];
 
-function word_reverse(word, ordinata) {
+function word_reverse(word, ordinata, reverse) {
     for (let i = 0; i < word.length; i++) {
         const carattere = word[i];
         ordinata.push(carattere)
         
     }
 
+    for (let i = word.length; i !== -1; i--) {
+        const carattere = word[i];
+        reverse.push(carattere)
+    }
 }
 
-word_reverse(stringa,ordinata);
+word_reverse(stringa,ordinata,reverse);
 console.log(ordinata);
+console.log(reverse);
